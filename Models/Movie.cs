a6b4fe31c7ec3cee5e88020ce31bfee0cf8920ce
Remon <remon.onnorokom@gmail.com>
2025-04-08 14:40:23 +0600

    just add the model(movie) class

---
+++ Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Learn.AspNetCore.Basic.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]  //Data annotation
+        public DateTime ReleaseDate { get; set; }
+        public string? Genere { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
