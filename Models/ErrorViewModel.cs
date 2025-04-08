f228e7943b6ad79a9c75adb9097aa951ce0b9efb
Remon <remon.onnorokom@gmail.com>
2025-04-08 14:27:04 +0600

    Create a Initial Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn.AspNetCore.Basic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
